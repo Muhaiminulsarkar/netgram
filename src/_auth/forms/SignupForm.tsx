03952eb5db71a19cc86dea9808ad827452023040
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-03-13 00:02:00 +0600

    update

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -49,7 +49,7 @@ const SignupForm = () => {
                 <img src="/assets/images/logo.svg" alt="logo" />
 
                 <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-                <p className="text-light-3 small-medium md:base-regular mt-2">To use NETGRAM enter your details</p>
+                <p className="text-light-3 small-medium md:base-regular mt-2">To use NETGRAM please enter your details</p>
 
                 <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
                     <FormField
