239ff2bf0c619b61df6e85658136c0c8cd87761d
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-04-02 23:27:36 +0600

    problem solved

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,12 +1,14 @@
+
 import {
-    useQuery,
+
     useMutation,
     useQueryClient,
-    useInfiniteQuery,
+
 
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
+import { createUserAccount, signInAccount, signOutAccount, createPost } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -26,5 +28,17 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
+    });
+}
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            });
+        }
     });
 }
