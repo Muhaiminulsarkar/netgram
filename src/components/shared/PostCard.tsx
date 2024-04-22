95fe7e25e9a3ef126b17d7f493d200894f1a3923
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-04-22 23:55:30 +0600

    post card updated

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -42,7 +42,7 @@ const PostCard = ({ post }: PostCardProps) => {
         <Link to={`/update-post/${post.$id}`}
           className={`${user.id !== post.creator.$id && "hidden"}`}
         >
-          <img src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
+          <img src="/assets/icons/edit.svg" alt="edit" width={21} height={21} />
         </Link>
       </div>
       <Link to={`/posts/${post.$id}`}>
