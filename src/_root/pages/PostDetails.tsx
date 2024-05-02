f6f1137fd405318d23b58f6fa5954d8679c8c429
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-05-03 01:18:20 +0600

    updating

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,4 +1,5 @@
 import Loader from "@/components/shared/Loader";
+import { Button } from "@/components/ui/button";
 import { useUserContext } from "@/context/AuthContext";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import { formatDateString } from "@/lib/utils";
@@ -8,6 +9,7 @@ const PostDetails = () => {
   const { id } = useParams()
   const { data: post, isPending } = useGetPostById(id || '');
   const { user } = useUserContext();
+  const handleDeletePost = () => { }
 
   return (
     <div className="post_details-container">
@@ -25,7 +27,7 @@ const PostDetails = () => {
                 <img
                   src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'}
                   alt="creator"
-                  className="rounded-full w-12 lg:h-12"
+                  className="rounded-full w-8 h-8 lg:w-12 lg:h-12"
                 />
                 <div className="flex flex-col">
                   <p className="base-medium lg:body-bold text-light-1">
@@ -42,16 +44,37 @@ const PostDetails = () => {
                   </div>
                 </div>
               </Link>
-              <div className="flex-center gap-4">
+              <div className="flex-center gap-2">
                 <Link to={`/update-post/${post?.$id}`} className={`
                 ${user.id !== post?.creator.$id && 'hidden'}`}>
                   <img src="/assets/icons/edit.svg" width={24} height={24} alt="edit" />
                 </Link>
+                <Button
+                  onClick={handleDeletePost}
+                  variant="ghost"
+                  className={`ghost_details-delete_btn ${user.id !== post?.creator.$id && 'hidden'}`}
+                >
+                  <img
+                    src="/assets/icons/delete.svg"
+                    alt="delete"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
               </div>
-
-
             </div>
+            <hr className="border w-full border-dark-4/80" />
 
+            <div className="small-medium lg:base-medium py-5">
+              <p>{post?.caption}</p>
+              <ul className="flex gap-1 mt-2">
+                {post?.tags.map((tag: string) => (
+                  <li key={tag} className="text-light-3">
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       )}
